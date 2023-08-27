97db5c4501fcdf9d419e93aa5fd20a302829baaf
Chiu-Chou Lin <dsobscure@outlook.com>
2023-08-27 19:47:21 +0800

    Update setup.py

--- platform/setup.py
+++ platform/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='cgi_drl',
       version='0.1.0',
       install_requires=[
-            'setuptools==59.4.0',
+            'setuptools<58.0.0',
             'pyyaml==6.0',
             'gym==0.12.0',
             'atari-py==0.2.9',
