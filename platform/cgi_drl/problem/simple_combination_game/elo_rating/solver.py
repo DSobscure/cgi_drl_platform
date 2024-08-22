a89c4053161c4ec66cef21adbe165f7bbbdfbc0f
DSobscure <dsobscure@outlook.com>
2024-08-22 20:14:57 +0800

    TMLR Game Balance Measures code backup3
    all problem files (solver and config) for toy games
    including:
    - Simple Combination Game
    - Rock-Paper-Scissors
    - Advanced Combination Game

---
+++ platform/cgi_drl/problem/simple_combination_game/elo_rating/solver.py
@@ -0,0 +1,29 @@
+from cgi_drl.problem.workflow_template.elo_rating_template import EloRatingSolver
+
+def launch(problem_config):
+    load = problem_config["load_function"]
+    # setup model
+    elo_rating_config = load(*problem_config["elo_rating"])
+    problem_config["elo_rating"] = elo_rating_config
+    from cgi_drl.measure_model.elo_rating.trainer import EloRatingTrainer
+    model = EloRatingTrainer(elo_rating_config)
+
+    # setup record
+    from cgi_drl.data_storage.match_records.simple_combination_game_tabular_record import SimpleCombinationGameMatchResultBuffer
+
+    record_train_config = load(*problem_config["record_train"])
+    problem_config["record_train"] = record_train_config
+    result_buffer_train = SimpleCombinationGameMatchResultBuffer(record_train_config)
+
+    record_test_config = load(*problem_config["record_test"])
+    problem_config["record_test"] = record_test_config
+    result_buffer_test = SimpleCombinationGameMatchResultBuffer(record_test_config)
+    
+    # setup solver
+    problem_config["solver"] = problem_config
+    solver = EloRatingSolver(problem_config)
+    solver.model = model
+    solver.result_buffer_train = result_buffer_train
+    solver.result_buffer_test = result_buffer_test
+
+    solver.train()
